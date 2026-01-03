b6dff2e4818ca24dfe73f7ea45be01c599f781e7
Harsh Singh <hrsharshsingh@gmail.com>
2026-01-03 16:54:45 +0530

    Add EXTERNAL_LLM Notification

---
+++ nlp-service/nlpService/src/test/java/bank/nlpService/NlpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.nlpService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
